24ac6a5c0850de695eab44b0a7e5322e2b758b2a
wangshuo <wangshuo@bhz.com.cn>
2016-10-21 17:59:57 +0800

    增加BPOS启动测试类，方便本地测试时加载资源文件

---
+++ eps-client/src/test/java/com/bhz/eps/test/BPosEmulatorTest.java
@@ -0,0 +1,14 @@
+package com.bhz.eps.test;
+
+import com.bhz.eps.BPosEmulator;
+
+/**
+ * Created by wangshuo on 2016/10/11.
+ */
+public class BPosEmulatorTest {
+
+    public static void main(String[] args) {
+        new BPosEmulator().run();
+    }
+
+}
